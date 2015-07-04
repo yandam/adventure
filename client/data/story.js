e682a70e255c32d70c8cb99da7fcaad5e46529d5
Damien <damien@dam.io>
2015-07-04 18:36:44 +0200

    add main menu and select

--- client/data/story.js
+++ client/data/story.js
@@ -1,30 +1,42 @@
-__STORY__ = {
-  start: {
-      text:'Salut camille, yo!',
+__STORIES__ = {
+  'Camille':{
+    start: {
+        text:'Salut camille, yo!',
+        choices: [
+          {next:2, message:"Oui?"},
+          {next:2, message:"Euuh.."},
+          {next:2, message:"C'est moi!"}
+        ]
+    },
+    2: {
+      text:"Mais attend, c'est qui ton ennemi juré ?",
       choices: [
-        {next:2, message:"Oui?"},
-        {next:2, message:"Euuh.."},
-        {next:2, message:"C'est moi!"}
-      ]
+          {next:4, message:'Melon, ce pianotateur'},
+          {next:3, message:'Carotte, cette pianiste'},
+          {next:3, message:'AHAHAHAHAHAH'}
+        ]
+    },
+    3: {
+      text:"Et ben c'est fini! Voila!",
+      choices: [
+          {next:"start", message:'Je veux re-essayer!'},
+        ]
+    },
+    4: {
+      text:"Mwahahahahahah!",
+      choices: [
+          {next:3, message:'Quoi ? Que va tu faire ?'},
+        ]
+    },
   },
-  2: {
-    text:"Mais attend, c'est qui ton ennemi juré ?",
-    choices: [
-        {next:4, message:'Melon, ce pianotateur'},
-        {next:3, message:'Carotte, cette pianiste'},
-        {next:3, message:'AHAHAHAHAHAH'}
-      ]
-  },
-  3: {
-    text:"Et ben c'est fini! Voila!",
-    choices: [
-        {next:"start", message:'Je veux re-essayer!'},
-      ]
-  },
-  4: {
-    text:"Mwahahahahahah!",
-    choices: [
-        {next:3, message:'Quoi ? Que va tu faire ?'},
-      ]
-  },
+  'Tea time':{
+    'start':{
+        text:'Do you want some tea with your madeleine ?',
+        voice:'UK English Male',
+        choices: [
+          {next:2, message:"Yes, sir!"},
+          {next:2, message:"Do you have coffee ?"},
+        ]
+    }
+  }
 }
